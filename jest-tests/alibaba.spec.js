a37c0c3ef0521adcd3819d58a2e54bbfb6b8547e
himanshiLt <89007259+himanshiLt@users.noreply.github.com>
2021-11-16 15:57:20 +0530

    Remove all tests to be blocklisted

--- jest-tests/alibaba.spec.js
+++ jest-tests/alibaba.spec.js
@@ -13,20 +13,20 @@ beforeAll(async () => {
 })
 
 describe('Alibaba Search', () => {
-  test.skip('has search input', async () => {
-    await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('https://www.alibaba.com', { waitUntil: 'networkidle0' })
-    const searchInput = await page.$('input.ui-searchbar-keyword')
-    expect(searchInput).toBeTruthy()
-  }, 10000)
+//   test.skip('has search input', async () => {
+//     await page.setViewport({ width: 1280, height: 800 })
+//     await page.goto('https://www.alibaba.com', { waitUntil: 'networkidle0' })
+//     const searchInput = await page.$('input.ui-searchbar-keyword')
+//     expect(searchInput).toBeTruthy()
+//   }, 10000)
 
-  test('shows search results after search input', async () => {
-    await page.type('input.ui-searchbar-keyword', 'lucky cat')
-    await page.click('input.ui-searchbar-submit')
-    await page.waitForSelector('[data-content="abox-ProductNormalList"]')
-    const firstProduct = await page.$('.item-content')
-    expect(firstProduct).toBeTruthy()
-  })
+//   test('shows search results after search input', async () => {
+//     await page.type('input.ui-searchbar-keyword', 'lucky cat')
+//     await page.click('input.ui-searchbar-submit')
+//     await page.waitForSelector('[data-content="abox-ProductNormalList"]')
+//     const firstProduct = await page.$('.item-content')
+//     expect(firstProduct).toBeTruthy()
+//   })
 
   afterAll(async () => {
     await browser.close()
